6fef140ac9b4ba9b2803f68711f19f2f77bcffea
Soham Patkar <134045535+SohamPatkar@users.noreply.github.com>
2025-03-13 19:36:39 +0530

    Add files via upload

---
+++ InsertionSort/InsertionSort/InsertionSort.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+// Function to sort the array using insertion sort algorithm.
+void processInsertionSort(int arr[], int n) {
+    
+    for (int i = 1; i < n; i++) {
+        int key = arr[i]; 
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j]; 
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+void displayArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+// Function to dynamically allocate an array and fill it with random values.
+void fillDynamicArrayWithRandomValues(int** arr, int* n) {
+    cout << "Enter the size of the array: ";
+    cin >> *n;
+    *arr = new int[*n];
+    srand(time(0)); // Seed for random number generation
+    for (int i = 0; i < *n; i++) {
+        (*arr)[i] = rand() % 1000; // Fill with random numbers between 0 and 999
+    }
+}
+
+int main() {
+    int* arr;
+    int n;
+    fillDynamicArrayWithRandomValues(&arr, &n);
+    cout << "Unsorted array: ";
+    displayArray(arr, n);
+    processInsertionSort(arr, n);
+    cout << "Sorted array: ";
+    displayArray(arr, n);
+    delete[] arr; // Deallocate dynamically allocated memory
+    return 0;
+}
